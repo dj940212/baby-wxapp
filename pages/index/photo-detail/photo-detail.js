6f8fd4b9714af84f9e0e3c812671c627c8138fd2
dj940212 <2902273280@qq.com>
2017-07-19 17:23:00 +0800

    photo-detail跳转

--- pages/index/photo-detail/photo-detail.js
+++ pages/index/photo-detail/photo-detail.js
@@ -2,7 +2,7 @@ var app = getApp()
 Page({
   data: {
     imgUrls: [],
-    indicatorDots: true,
+    indicatorDots: false,
     vertical: false,
     autoplay: false,
     interval: 3000,
@@ -11,7 +11,12 @@ Page({
   },
   onLoad: function() {
     this.setData({
-      imgUrls: app.photoVideoList[app.photoVideoIndex].photoVideoUrl
+      // imgUrls: app.photoVideoList[app.photoVideoIndex].photoVideoUrl
+      imgUrls:[
+        "http://ot2nmqx5r.bkt.clouddn.com/tmp_1984021442o6zAJs0qJDggmNsKmIogeSi9wsLQfa63f01a62200b0d1e1e997a3131d150.jpg",
+        "http://ot2nmqx5r.bkt.clouddn.com/tmp_1984021442o6zAJs0qJDggmNsKmIogeSi9wsLQ344821469d09a50e999e37741dd9ae9b.jpg",
+        "http://ot2nmqx5r.bkt.clouddn.com/tmp_1984021442o6zAJs0qJDggmNsKmIogeSi9wsLQ5afc0d0cdc1158b7ba679061d0055b9a.jpg",
+        "http://ot2nmqx5r.bkt.clouddn.com/tmp_1984021442o6zAJs0qJDggmNsKmIogeSi9wsLQ3653d01e315e2ca186d8d933b22bead6.jpeg"]
     })
     console.log(this.data.imgUrls)
     this.getSwiperHeight()
@@ -29,6 +34,13 @@ Page({
         console.log('windowHeight: ' + windowHeight)  
 
       }  
-  })  
-  }
+    })  
+  },
+  // 跳转到photo-detail页
+  back: function() {
+    wx.navigateTo({
+      url: '../index'
+    })
+    // this.photoVideoIndex = e.currentTarget.dataset.index
+  },
 })
